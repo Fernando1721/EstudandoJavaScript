39e0dc7dd9abcdf76167c551fb3a4b976662c8b2
FERNANDO MIRANDA DA SILVA SANTOS <fernando.mssantos2@senacsp.edu.br>
2022-05-02 16:42:14 -0300

    Aprendendo um de front e javascript

--- 9-28042022/index.js
+++ 9-28042022/index.js
@@ -22,11 +22,17 @@ const express = require('express');
 // importação do módulo mongoose
 const mongoose = require('mongoose');
 
+// importação do modulo do cors
+const cors = require('cors');
+
 // criação do app referente ao express
 const app = express();
 
 // preparar o servidor para receber json
 app.use(express.json());
+
+// Uso do cors para desbloqueio de acesso
+app.use(cors());
 
 
 /*
@@ -57,13 +63,19 @@ const default_route = "/api/cliente";
 
 // rota para listar os clientes com endpoint listar
 app.get(`${default_route}/listar`,(req,res)=>{
-    res.status(200).send({output:`Rota GET`});
+    
+    Cliente.find().then((dados)=>{
+       
+        res.status(200).send({output:dados});
+    })
+    .catch((erro)=> res.status(500).send({output:`Erro interno ao processar a consulta -> ${erro}`}));
+    
 });
 
 // rota para cadastrar os clientes com endpoint cadastrar
 app.post(`${default_route}/cadastrar`,(req,res)=>{
     
-    const cli = new Cliente(req,body);
+    const cli = new Cliente(req.body);
     cli.save().then((dados)=>{
         res.status(201).send({output:`Cadastro realizado`, payload:dados})
     }).catch((erro)=>console.error(`Erro ao tentar cadastrar ${erro}`));
@@ -75,14 +87,29 @@ app.post(`${default_route}/cadastrar`,(req,res)=>{
 // rota para atualizar os clientes com endpoint atualizar
 // passagem de argumentos pela url com o id do cliente
 app.put(`${default_route}/atualizar/:id`,(req,res)=>{
-    res.status(200).send({output:req.params.id});
+   
+    Cliente.findByIdAndUpdate(req.params.id,req.body,{new:true},(erro,dados)=> {
+        if(erro){
+            return res.status(500).send({output: `Não atualizou -> ${erro}`});
+        }
+        res.status(200).send({output:"Dados atualizados"})
+    });
+
 });
 
 // rota para apagar clientes com endpoint deletar
 app.delete(`${default_route}/apagar/:id`,(req,res)=>{
-    res.status(204).send({output: req.params.id});
+    
+    Cliente.findByIdAndDelete(req.params.id, (erro,dados)=>{
+        if(erro){
+            return res.status(500).send({output:`Erro ao tentar apagar -> ${erro}`});
+        }
+        res.status(204).send({output:`Apagar`})
+    });
 });
 
 // definir a porta de comunicação do servidor
+
+
 app.listen(8088
     ,()=>console.log("Servidor online em http://localhost:8088"));
